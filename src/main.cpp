d3c0534e2598ca8d60b3c9082209aecfa3981e6a
IrichRin <chad.flores17@gmail.com>
2025-03-23 16:38:08 +0800

    RTC pins comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Clock.h>
-                                     
+
+                    //RTC PINS = CLK(3), DAT(4), RST(5)     
 Clock clock(new virtuabotixRTC(3, 4, 5), new int[7]{0, 1, 2, 6, 7, 8, 9}, new int[4]{10, 11, 12, 13});
 
 void setup()  {      
